9b14a4af62f78e11ddef5ea77cc7632776a6f428
Kelvin Wauchope <kwauchope@users.noreply.github.com>
2020-06-23 20:50:06 +1000

    changes required for a build, do not merge

---
+++ tasmota/user_config_override.h
@@ -0,0 +1,15 @@
+//Setup what we need
+
+#ifndef USE_ENERGY_SENSOR
+#define USE_ENERGY_SENSOR
+#endif
+
+#ifndef USE_SPI
+#define USE_SPI
+#endif
+
+#ifndef USE_ATM90E26
+#define USE_ATM90E26
+#endif
+
+//Disable a bunch of defaults
