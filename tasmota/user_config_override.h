05be25be3ef81d169930636dfdebbb906d542110
Kelvin Wauchope <kwauchope@users.noreply.github.com>
2020-06-24 02:23:52 +1000

    Add display

--- tasmota/user_config_override.h
+++ tasmota/user_config_override.h
@@ -12,4 +12,20 @@
 #define USE_ATM90E26
 #endif
 
+#ifndef USE_I2C
+#define USE_I2C
+#endif
+
+#ifndef USE_DISPLAY
+#define USE_DISPLAY
+#endif
+
+#ifndef USE_DISPLAY_MODES1TO5
+#define USE_DISPLAY_MODES1TO5                // Enable display mode 1 to 5 in addition to mode 0
+#endif
+
+#ifndef USE_DISPLAY_SSD1306
+#define USE_DISPLAY_SSD1306                  // [DisplayModel 2] [I2cDriver4] Enable SSD1306 Oled 128x64 display (I2C addresses 0x3C and 0x3D) (+16k code)
+#endif
+
 //Disable a bunch of defaults
